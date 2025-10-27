e28db12d7e493042fe55eb45dd654a20bd89cd29
Azkraft <azanov-2004@mail.ru>
2025-10-27 19:00:14 +0500

    fix(ObjectComparison): migrate to FluentAssertions

--- Testing/Basic/Homework/1. ObjectComparison/ObjectComparison.cs
+++ Testing/Basic/Homework/1. ObjectComparison/ObjectComparison.cs
@@ -1,9 +1,12 @@
-﻿using NUnit.Framework;
+﻿using FluentAssertions;
+using NUnit.Framework;
 using NUnit.Framework.Legacy;
 
 namespace HomeExercise.Tasks.ObjectComparison;
 public class ObjectComparison
 {
+	private const int MaxGenDepth = 1000;
+
     [Test]
     [Description("Проверка текущего царя")]
     [Category("ToRefactor")]
@@ -12,19 +15,23 @@ public class ObjectComparison
         var actualTsar = TsarRegistry.GetCurrentTsar();
 
         var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
-            new Person("Vasili III of Russia", 28, 170, 60, null));
+            new Person("Vasili III of Russia", 28, 170, 60, null!));
 
-        // Перепишите код на использование Fluent Assertions.
-        ClassicAssert.AreEqual(actualTsar.Name, expectedTsar.Name);
-        ClassicAssert.AreEqual(actualTsar.Age, expectedTsar.Age);
-        ClassicAssert.AreEqual(actualTsar.Height, expectedTsar.Height);
-        ClassicAssert.AreEqual(actualTsar.Weight, expectedTsar.Weight);
+        var currentActual = actualTsar;
+        var currentExpected = expectedTsar;
+        for (var i = 0; i > -MaxGenDepth; i--)
+        {
+	        if (currentExpected is null && currentActual is null)
+		        return;
 
-        ClassicAssert.AreEqual(expectedTsar.Parent!.Name, actualTsar.Parent!.Name);
-        ClassicAssert.AreEqual(expectedTsar.Parent.Age, actualTsar.Parent.Age);
-        ClassicAssert.AreEqual(expectedTsar.Parent.Height, actualTsar.Parent.Height);
-        ClassicAssert.AreEqual(expectedTsar.Parent.Parent, actualTsar.Parent.Parent);
-    }
+            CheckTsarFields(currentActual, currentExpected, i);
+
+            currentActual = currentActual?.Parent;
+            currentExpected = currentExpected?.Parent;
+        }
+
+		Assert.Fail("The verification depth has reached its maximum value.");
+	}
 
     [Test]
     [Description("Альтернативное решение. Какие у него недостатки?")]
@@ -32,13 +39,23 @@ public class ObjectComparison
     {
         var actualTsar = TsarRegistry.GetCurrentTsar();
         var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
-            new Person("Vasili III of Russia", 28, 170, 60, null));
+            new Person("Vasili III of Russia", 28, 170, 60, null!));
 
         // Какие недостатки у такого подхода? 
+
+        /*
+         * Мы не получаем полезной информации при падении теста.
+         * Было бы полезно знать в каком поле наблюдается различие.
+         * Также мы не знаем в каком поколении находится это различие.
+         * Кроме того, реализация AreEqual рекурсивная
+         * и при этом не защищена от замыкания (зацикливания) цепочки родословной.
+         * Несколько лучшим решением было бы реализовать класс с интерфейсом IEqualityComparer<Person>.
+         */
+
         ClassicAssert.True(AreEqual(actualTsar, expectedTsar));
     }
 
-    private bool AreEqual(Person? actual, Person? expected)
+    private bool AreEqual(Person? actual, Person? expected) 
     {
         if (actual == expected) return true;
         if (actual == null || expected == null) return false;
@@ -48,5 +65,23 @@ public class ObjectComparison
             && actual.Height == expected.Height
             && actual.Weight == expected.Weight
             && AreEqual(actual.Parent, expected.Parent);
+    }
+
+    private void CheckTsarFields(Person? actual, Person? expected, int genNumber)
+    {
+	    var message = $"the generation {genNumber}";
+
+	    if (expected is null)
+	    {
+		    actual.Should().BeNull(message);
+		    return;
+	    }
+
+		actual.Should().NotBeNull(message);
+	    expected.Should().NotBeNull(message);
+	    actual!.Name.Should().Be(expected!.Name, message);
+		actual!.Age.Should().Be(expected!.Age, message);
+		actual!.Height.Should().Be(expected!.Height, message);
+		actual!.Weight.Should().Be(expected!.Weight, message);
     }
 }
